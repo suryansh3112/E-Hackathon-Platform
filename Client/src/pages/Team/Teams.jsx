a4c66a3a9046398f4cf15964ebbd1e561d46b0c7
Suryansh Purohit <suryansh.purohit3112@gmail.com>
2022-01-30 20:32:26 +0530

    Updated teams page

--- Client/src/pages/Team/Teams.jsx
+++ Client/src/pages/Team/Teams.jsx
@@ -1,38 +1,52 @@
-import React, { useState, useEffect } from "react";
-import { makeStyles } from "@mui/styles";
-import { getAllTeams, createTeam, joinTeam } from "./utils";
-import { useAuth } from "../../contexts/AuthContext";
-import { useSnackbar } from "../../contexts/SnackbarContext";
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
-import Constants from "../../common/Constants";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import { Link, useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@mui/styles';
+import { getAllTeams, createTeam, joinTeam } from './utils';
+import { useAuth } from '../../contexts/AuthContext';
+import { useSnackbar } from '../../contexts/SnackbarContext';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Constants from '../../common/Constants';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import { Link, useNavigate } from 'react-router-dom';
+import { Card } from '../../components';
+import GroupsOutlinedIcon from '@mui/icons-material/GroupsOutlined';
 
 const useStyles = makeStyles({
   root: {
-    display: "flex",
-    // justifyContent: "center",
-    // alignItems: "center",
+    flexGrow: 1,
+    margin: 20,
+  },
+  wrapper: {
+    display: 'flex',
+    justifyContent: 'center',
   },
   flexOne: {
     flex: 1,
   },
   flexAlign: {
-    display: "flex",
-    alignItems: "center",
+    display: 'flex',
+    alignItems: 'center',
+  },
+  orText: {
+    fontSize: 20,
+    fontWeight: 600,
+    margin: '20px 0',
+    textAlign: 'center',
+  },
+  btn: {
+    marginTop: 10,
   },
 });
 export default function Teams() {
   const classes = useStyles();
   const { userData } = useAuth();
   const [teams, setTeams] = useState([]);
-  const [teamName, setTeamName] = useState("");
-  const [teamCode, setTeamCode] = useState("");
+  const [teamName, setTeamName] = useState('');
+  const [teamCode, setTeamCode] = useState('');
   const { showSnackbar } = useSnackbar();
   const navigate = useNavigate();
 
@@ -57,7 +71,7 @@ export default function Teams() {
     } catch (error) {
       const message =
         error?.response?.data?.message || Constants.strings.somethingWentWrong;
-      showSnackbar(message, "error");
+      showSnackbar(message, 'error');
     }
   };
   const handleJoin = async () => {
@@ -67,65 +81,59 @@ export default function Teams() {
     } catch (error) {
       const message =
         error?.response?.data?.message || Constants.strings.somethingWentWrong;
-      showSnackbar(message, "error");
+      showSnackbar(message, 'error');
     }
   };
-
   return (
     <div className={classes.root}>
-      <div className={classes.flexOne}>
-        <h1>Teams</h1>
-        {teams?.length > 0 ? (
-          <List>
-            {teams.map((team, idx) => {
-              return (
-                <ListItem key={idx}>
-                  <ListItemButton component={Link} to={`/teams/${team.id}`}>
-                    <ListItemText primary={team.name} />
-                  </ListItemButton>
-                </ListItem>
-              );
-            })}
-          </List>
-        ) : (
-          <h1>No Teams Found</h1>
-        )}
-      </div>
-      <div className={classes.flexOne}>
-        <div className={classes.flexAlign}>
+      <div className={classes.wrapper}>
+        <Card>
+          <h1>Teams</h1>
+          {teams?.length > 0 ? (
+            <List>
+              {teams.map((team, idx) => {
+                return (
+                  <ListItem key={idx} disablePadding>
+                    <ListItemButton component={Link} to={`/teams/${team.id}`}>
+                      <ListItemIcon>
+                        <GroupsOutlinedIcon />
+                      </ListItemIcon>
+                      <ListItemText primary={team.name} />
+                    </ListItemButton>
+                  </ListItem>
+                );
+              })}
+            </List>
+          ) : (
+            <h1>No Teams Found</h1>
+          )}
+        </Card>
+        <Card>
           <TextField
             required
-            label="Team Name"
+            label='Team Name'
             value={teamName}
             onChange={(e) => setTeamName(e.target.value)}
-            margin="normal"
-          />
-
-          <Button
             className={classes.btn}
-            onClick={handleCreate}
-            variant="contained"
-          >
-            Create Team
-          </Button>
-        </div>
-        <div className={classes.flexAlign}>
+          />
+          <div className={classes.btn}>
+            <Button onClick={handleCreate} variant='contained' fullWidth>
+              Create Team
+            </Button>
+          </div>
+          <p className={classes.orText}>OR</p>
           <TextField
             required
-            label="Team Code"
+            label='Team Code'
             value={teamCode}
             onChange={(e) => setTeamCode(e.target.value)}
-            margin="normal"
           />
-
-          <Button
-            className={classes.btn}
-            onClick={handleJoin}
-            variant="contained"
-          >
-            Join Team
-          </Button>
-        </div>
+          <div className={classes.btn}>
+            <Button onClick={handleJoin} variant='contained' fullWidth>
+              Join Team
+            </Button>
+          </div>
+        </Card>
       </div>
     </div>
   );
